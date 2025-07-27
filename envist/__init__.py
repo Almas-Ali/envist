3d5a75c809e32f7fe77aa6405614f54626f0eb74
Md. Almas Ali <almaspr3@gmail.com>
2025-07-27 20:25:03 +0600

    chore: bump version to 0.1.2

--- envist/__init__.py
+++ envist/__init__.py
@@ -87,7 +87,7 @@ from .core.exceptions import (
 from .core.parser import Envist
 from .validators import validator
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ["Envist"]
 __author__ = "Md. Almas Ali"
 __all__ = [
